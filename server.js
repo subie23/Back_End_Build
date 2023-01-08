691fd88387a892d6a45275fade257cf4fbf9a5bf
Suzanne Banasihan <sjb04@ymail.com>
2023-01-08 02:20:40 -0800

    updated connection

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./routes/api');
 // import sequelize connection
 const sequelize = require('./config/connection');
 
