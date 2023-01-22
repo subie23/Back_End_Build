3e2feebe91a2d688710f1db9d205cd3ee38e8b94
Suzanne Banasihan <sjb04@ymail.com>
2023-01-21 23:16:03 -0800

    added line 1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const express = require('express');
 const routes = require('./routes/api');
-// import sequelize connection
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
